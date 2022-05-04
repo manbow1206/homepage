efdb203dc3254423a33feebcf8bff390b52e9b66
manbow1206 <tyasunaga@iyell.jp>
2022-05-04 09:23:09 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Container, Box } from "@chakra-ui/react";
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in Japan!
+      </Box>
+    </Container>
+
+  )
+  
+};
+export default Page;
