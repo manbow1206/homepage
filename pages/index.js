f077bd4e5014df6b2a556aac4bfeaf4b85e1c93b
manbow1206 <tyasunaga@iyell.jp>
2022-05-04 09:43:47 +0900

    imageをお借りします。

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, Heading } from "@chakra-ui/react";
 
 const Page = () => {
   return (
@@ -6,9 +6,17 @@ const Page = () => {
       <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
         Hello, I&apos;m a full-stack developer based in Japan!
       </Box>
+
+       <Box display={{md : 'flex'}}>
+         <Box flexGlow={1}>
+           <Heading as="h2" variant="page-title">
+             Takuya Yasunaga
+           </Heading>
+           <p>Digital Craftzman (Artist / Developer / Designer)</p>
+         </Box>
+       </Box>
     </Container>
 
   )
-  
 };
 export default Page;
