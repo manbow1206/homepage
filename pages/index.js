a3c6c9e49ef49e07f56cddc9ea024a203a90930e
manbow1206 <tyasunaga@iyell.jp>
2022-05-05 11:55:15 +0900

    add : Pragraph

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import NextLink from 'next/link'
 import {
   Container,
   Box,
   Heading,
   Image,
+  Link,
   useColorModeValue,
+  Button,
 } from "@chakra-ui/react";
-import Section from "../components/section"
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import Section from "../components/section";
+import Paragraph from "../components/paragraph";
 
 const Page = () => {
   return (
@@ -48,7 +53,24 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of{' '}<NextLink href="#"><Link>Manbow</Link></NextLink> .
+        </Paragraph>
+        <Box align="right" my={4}>
+          <NextLink href="/work">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Profile
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
     </Container>
   );
