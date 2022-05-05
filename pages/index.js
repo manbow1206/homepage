28b2f71f9c24a80634b24adf5010917adcdd2260
manbow1206 <tyasunaga@iyell.jp>
2022-05-05 11:17:32 +0900

    format : index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Section from "../components/section"
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in Japan!
       </Box>
 
@@ -20,7 +33,15 @@ const Page = () => {
           ml={{ md: 6 }}
           align="center"
         >
-          <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="2xl" src="/images/avatar.png"></Image>
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="2xl"
+            src="/images/avatar.png"
+          ></Image>
         </Box>
       </Box>
     </Container>
